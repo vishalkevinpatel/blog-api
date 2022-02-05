a664f9a72bf112be702fbfa7bcfd43804fab4045
Peter Jang <peter.x.jang@gmail.com>
2022-02-05 15:31:39 -0600

    Sort posts by id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user, except: [:index, :show]
 
   def index
-    posts = Post.all
+    posts = Post.all.order(:id)
     render json: posts
   end
 
